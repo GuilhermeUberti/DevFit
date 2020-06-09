1f7333688eca8c2b07c613c9dfb6a7074469ab14
Guilherme Uberti <guilherme.uberti@hotmail.com>
2020-06-09 10:43:02 -0300

    Home header e legendas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,119 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components/native';
-import { Text } from 'react-native';
+import { connect } from 'react-redux';
 
-const Container = styled.View`
-    font-size: 18px;
+import HomeMonthScroll from '../components/HomeMonthScroll';
+import HomeDaysScroll from '../components/HomeDaysScroll';
+import HomeDayStatus from '../components/HomeDayStatus';
+
+const Container = styled.SafeAreaView`
+    align-items: center;
 `;
 
-export default () => {
+const Legend = styled.View`
+    width: 90%;
+    align-items: flex-start;
+    margin-top: 30px;
+`;
+
+const LegendText = styled.Text`
+    color: #555;
+`;
+
+const LengendItem = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-top: 5px;
+`;
+
+const LegendBox = styled.View`
+    width: 15px;
+    height: 15px;
+    background-color: #CCC;
+    margin-right: 5px;
+`;
+
+const Page = (props) => {
+
     return (
         <Container>
-            <Text>HomePage</Text>
+            <HomeMonthScroll />
+            <HomeDaysScroll />
+            <HomeDayStatus />
+
+            <Legend>
+                <LegendText>Legenda:</LegendText>
+                <LengendItem>
+                    <LegendBox style={{ backgroundColor: '#B5EEFF' }}></LegendBox>
+                    <LegendText>Hoje</LegendText>
+                </LengendItem>
+                <LengendItem>
+                    <LegendBox style={{ backgroundColor: '#B5FFB8' }}></LegendBox>
+                    <LegendText>Treino Feito</LegendText>
+                </LengendItem>
+                <LengendItem>
+                    <LegendBox style={{ backgroundColor: '#FFB5B5' }}></LegendBox>
+                    <LegendText>Treino Perdido</LegendText>
+                </LengendItem>
+                <LengendItem>
+                    <LegendBox style={{ backgroundColor: '#CCC', opacity: 0.2 }}></LegendBox>
+                    <LegendText>Dia de descanso</LegendText>
+                </LengendItem>
+                <LengendItem>
+                    <LegendBox style={{ backgroundColor: '#CCC' }}></LegendBox>
+                    <LegendText>Dia Futuro</LegendText>
+                </LengendItem>
+            </Legend>
         </Container>
     );
 }
+
+Page.navigationOptions = ({ navigation }) => {
+
+    const ConfigButtonArea = styled.TouchableHighlight`
+        width: 30px;
+        height: 30px;
+        justify-content: center;
+        align-items: center;
+    `;
+
+    const ConfigButtonImage = styled.Image`
+        width: 25px;
+        height: 25px;
+    `;
+
+    const ConfigButton = () => {
+
+        const btnAction = () => {
+            navigation.navigate('HomeConfig');
+        }
+
+        return (
+            <ConfigButtonArea onPress={btnAction}>
+                <ConfigButtonImage source={require('../assets/config.png')} />
+            </ConfigButtonArea>
+        );
+    }
+
+    return {
+        title: 'Seu progesso diário',
+        headerRight: <ConfigButton />,
+        headerRightContainerStyle: {
+            marginRight: 10
+        }
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Page);
